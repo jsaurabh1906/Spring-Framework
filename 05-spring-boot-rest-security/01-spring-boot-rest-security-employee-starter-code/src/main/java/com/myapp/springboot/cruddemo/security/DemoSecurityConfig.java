58326af466fbc38c7410a204c2b1474d4318e2c6
jsaurabh1906 <jsaurabh1906@gmail.com>
2023-06-03 19:59:50 +0530

    Spring-boot-rest-security-bcrypt-api-security-custom-tables

--- 05-spring-boot-rest-security/01-spring-boot-rest-security-employee-starter-code/src/main/java/com/myapp/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/01-spring-boot-rest-security-employee-starter-code/src/main/java/com/myapp/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -19,11 +19,30 @@ import javax.sql.DataSource;
 @Configuration
 public class DemoSecurityConfig {
 
-   //add support for jdbc
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        //define query to retrieve a user by username
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id , pw , active from members where user_id=?"
+        );
+
+        //define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id,role from roles where user_id=?"
+        );
+
+
+        return jdbcUserDetailsManager;
+    }
+
+
+   /*//add support for jdbc
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
         return new JdbcUserDetailsManager(dataSource);
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
