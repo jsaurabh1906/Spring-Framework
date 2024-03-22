a0740e9f33ed8c4b80758d5b9e8a1f6f1153c387
saurabh <jsaurabh1906@gmail.com>
2024-03-22 13:49:01 +0530

    Spring MVC Security - plain paaswords

--- 07-springboot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/myapp/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 07-springboot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/myapp/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -11,50 +11,50 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+  @Bean
+  public InMemoryUserDetailsManager userDetailsManager() {
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+    UserDetails john = User.builder()
+        .username("john")
+        .password("{noop}test123")
+        .roles("EMPLOYEE")
+        .build();
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+    UserDetails mary = User.builder()
+        .username("mary")
+        .password("{noop}test123")
+        .roles("EMPLOYEE", "MANAGER")
+        .build();
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+    UserDetails susan = User.builder()
+        .username("susan")
+        .password("{noop}test123")
+        .roles("EMPLOYEE", "MANAGER", "ADMIN")
+        .build();
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
+    return new InMemoryUserDetailsManager(john, mary, susan);
+  }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(configurer ->
-                        configurer
-                                .requestMatchers("/").hasRole("EMPLOYEE")
-                                .requestMatchers("/leaders/**").hasRole("MANAGER")
-                                .requestMatchers("/systems/**").hasRole("ADMIN")
-                                .anyRequest().authenticated()
-                )
-                .formLogin(form ->
-                        form
-                                .loginPage("/showMyLoginPage")
-                                .loginProcessingUrl("/authenticateTheUser")
-                                .permitAll()
-                )
-                .logout(logout-> logout.permitAll()
-                )
-                .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
+    http.authorizeHttpRequests(configurer ->
+            configurer
+                .requestMatchers("/").hasRole("EMPLOYEE")
+                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                .requestMatchers("/systems/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        )
+        .formLogin(form ->
+            form
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+        )
+        .logout(logout -> logout.permitAll()
+        )
+        .exceptionHandling(configurer -> configurer.accessDeniedPage("/access-denied"));
 
-        return http.build();
-    }
- }
+    return http.build();
+  }
+}
