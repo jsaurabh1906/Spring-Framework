13c5bb6102576273f0bf19bcbe306234109b2cbd
saurabh <jsaurabh1906@gmail.com>
2024-04-18 12:21:57 +0530

    Mapping on Collection - List

---
+++ 100-Hibernate - Advanced Development Techniques/hb-02-map-collections-list/src/main/java/com/myapp/hibernate/demo/CreateStudentImagesListDemo.java
@@ -0,0 +1,51 @@
+package com.myapp.hibernate.demo;
+
+import com.myapp.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+
+public class CreateStudentImagesListDemo {
+  public static void main(String[] args) {
+    //create session factory
+    SessionFactory factory = new Configuration()
+        . configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Student.class)
+        .buildSessionFactory();
+
+
+    //create session
+    Session session = factory.getCurrentSession();
+    try{
+      //create the objects
+      Student s1 = new Student("Saurabh","Jaiswal","ssj@gmail.com");
+      List<String> images = s1.getImages();
+
+      images.add("photo1.jpg");
+      images.add("photo2.jpg");
+      images.add("photo3.jpg"); //in sets , no duplicates allowed
+
+      //start the object
+      session.beginTransaction();
+
+      //save the object
+      System.out.println("Saving the Student and images...");
+      session.persist(s1);
+
+      //commit the transaction
+      session.getTransaction().commit();
+      System.out.println("Saved!!!");
+
+    }finally {
+      //clean up code
+      session.close();
+      factory.close();
+    }
+
+
+
+  }
+}
